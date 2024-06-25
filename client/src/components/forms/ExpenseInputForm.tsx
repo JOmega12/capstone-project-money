ab03489d5167c63d673ff93fe9aaabb09efd66f3
Jensen <omega90ej@gmail.com>
2024-06-25 11:35:59 -0700

    added more money provider skeleton structure

---
+++ client/src/components/forms/ExpenseInputForm.tsx
@@ -0,0 +1,8 @@
+
+
+export const ExpenseInputForm = () => {
+
+    return(
+        <></>
+    )
+}
--- client/src/providers/IncomeAndExpenseProvider.tsx
+++ client/src/providers/IncomeAndExpenseProvider.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useState } from "react";
+import { Dispatch, FormEvent, ReactNode, SetStateAction, createContext, useContext, useState } from "react";
 import { IncomeAndExpenseType } from "../types/types";
 
 type IncomeAndExpenseContextType = {
@@ -28,12 +28,69 @@ export const IncomeAndExpenseProvider = ({ children }: MoneyProviderProps) => {
 
     const [money, setMoney] = useState<IncomeAndExpenseType | null>(null);
     const [payHistory, setPayHistory] = useState([]);
+    const [incomeName, setIncomeName] = useState('');
+    const [totalIncomeAmount, setTotalIncomeAmount] = useState(0);
+    const [expenseName, setExpenseName] = useState('');
+    const [totalExpenseAmount, setTotalExpenseAmount] = useState(0);
     
     // console.log(money)
 
     // const refetch = () => {};
 
 
+    // date functions
+    const dateObj = new Date();
+    const day = dateObj.getUTCDate();
+    const year= dateObj.getUTCFullYear();
+    const monthName = dateObj.toLocaleString('default', {month: 'long'});
+    const pDay = day.toString().padStart(2,"0");
+    const newPaddedDate = `${monthName} ${pDay}, ${year}`
+
+    // *this creates new income
+    const createNewIncomeForm = () => {
+
+    }
+    
+    // *this create new expense
+    const createNewExpenseForm = () => {
+
+    }
+
+
+    const handleTotalMoneyCalculations = () => {
+      const totalMoney = totalIncomeAmount - totalExpenseAmount;
+    }
+
+
+
+    // *this creates the transaction receipt for income and adds to total amount
+    const handleIncomeFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const historyIncomeItems = {
+        incomeName: incomeName,
+        totalIncomeAmount: totalIncomeAmount,
+        incomeDate: newPaddedDate,
+      }
+
+      // ! i get a type error
+      // setPayHistory([...payHistory, historyItems])
+
+      // once submitted, resets all the values/states of the form
+
+    }
+
+    // *this creates the transaction receipt for expense  and adds to total amount
+    const handleExpenseFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const historyExpenseItems = {
+        expenseName: expenseName,
+        totalExpenseAmount: totalExpenseAmount,
+        expenseDate: newPaddedDate
+      }
+
+    }
 
   return (
   <MoneyContext.Provider 
