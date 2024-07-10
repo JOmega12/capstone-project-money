9ba5565939367bec3789f155eb5752584220b2ad
Jensen <omega90ej@gmail.com>
2024-07-09 21:09:14 -0700

    added model schemas to all endpoints

--- backend/budget_categories/models.py
+++ backend/budget_categories/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
+
+
+class Budget_categories(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models. CASCADE)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self): 
+        return f'{self.name}'
--- backend/user_budget_categories_junction/models.py
+++ backend/user_budget_categories_junction/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import CustomUser
+from budget_categories.models import Budget_categories
 
 # Create your models here.
+
+class UserBudgetCategoryJunction(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    budget_category = models.ForeignKey(Budget_categories, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.user} - {self.budget_category}"
