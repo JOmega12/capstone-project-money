66243da9b5517078dd9127cf09f98fc72c25c3f6
Jensen <omega90ej@gmail.com>
2024-07-14 00:13:31 -0700

    still learning views and urls

--- backend/transactions/urls.py
+++ backend/transactions/urls.py
@@ -4,7 +4,9 @@ from .views import TransactionList, TransactionDetail
 
 
 #! need to learn about the method .asview() and the syntax of the url patterns of path
+
+#as_view is for showing the information as a GET request 
 urlpatterns = [
-    path('api/', TransactionList.as_view(), name='transaction_list'),
-    path('api/<int:pk>', TransactionDetail.as_view(), name='transaction_detail')
+    path('money/', TransactionList.as_view(), name='transaction_list'),
+    path('money/<int:pk>', TransactionDetail.as_view(), name='transaction_detail')
 ]
