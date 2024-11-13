f9ec7bf894d4daa5c35f3d801a96e779849848df
Jensen <omega90ej@gmail.com>
2024-11-13 11:36:11 -0800

    trying out code to see for monthly

--- client/src/components/charts/IncomeExpenseCharts.tsx
+++ client/src/components/charts/IncomeExpenseCharts.tsx
@@ -13,6 +13,27 @@ let incomeData: number[] = [];
 let dates: string[] = [];
 
   if(Array.isArray(money)) {
+
+    // const monthlyData = money.reduce((acc, item) => {
+    //   const month = new Date(item.createdAt).toLocaleString("default", { month: "short", year: "numeric" });
+      
+    //   if (!acc[month]) {
+    //     acc[month] = { income: 0, expense: 0 };
+    //   }
+      
+    //   if (item.transactionType === 'income') {
+    //     acc[month].income += item.transactionAmount;
+    //   } else if (item.transactionType === 'expense') {
+    //     acc[month].expense += item.transactionAmount;
+    //   }
+
+    //   return acc;
+    // }, {} as Record<string, { income: number, expense: number }>);
+
+    // dates = Object.keys(monthlyData);
+    // incomeData = dates.map(month => monthlyData[month].income);
+    // expenseData = dates.map(month => monthlyData[month].expense);
+
     dates = money.map(item => item.createdAt);
     incomeData = money.filter(item => item.transactionType === 'income').map(item => item.transactionAmount);
     expenseData = money.filter(item => item.transactionType === 'expense').map(item => item.transactionAmount);
